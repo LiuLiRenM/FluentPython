47ad069497162726fa4949cc97fbea96dd01a7e1
Sakura <liuliren.mm@gmail.com>
2023-03-24 23:32:10 +0800

    添加doctest框架的另一个例子（带有更详细的说明文本）

--- doctest_simple.py
+++ doctest_simple.py
@@ -16,3 +16,23 @@ def my_function(a, b):
     :return: a * b 的结果
     """
     return a * b
+
+
+def my_function_another(a, b):
+    """
+    Returns a * b
+    Works with numbers:
+
+    >>> my_function(2, 3)
+    6
+
+    and strings:
+
+    >>> my_function('a', 3)
+    'aaa'
+
+    :param a:
+    :param b:
+    :return:
+    """
+    return a * b
