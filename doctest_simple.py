f4f5eb3eb74819c0fa83607dcd3a1d2ff16304a3
Sakura <liuliren.mm@gmail.com>
2023-04-22 22:42:43 +0800

    添加具名元组的doctest例子

--- doctest_simple.py
+++ doctest_simple.py
@@ -2,6 +2,7 @@
 doctest框架的使用案例
 
 """
+from collections import namedtuple
 
 
 def my_function(a, b):
@@ -169,5 +170,39 @@ def tuple_unpacking():
             print(fmt.format(name, lat, lon))
 
 
+def named_tuple():
+    """
+    具名元组
+
+    >>> from collections import namedtuple
+    >>> City = namedtuple('City', 'name country population coordinates')
+    >>> tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+    >>> tokyo
+    City(name='Tokyo', country='JP', population=36.933, coordinates=(35.689722, 139.691667))
+    >>> tokyo.population
+    36.933
+    >>> tokyo.coordinates
+    (35.689722, 139.691667)
+    >>> tokyo[1]
+    'JP'
+
+    >>> City._fields
+    ('name', 'country', 'population', 'coordinates')
+    >>> LatLong = namedtuple('LatLong', 'lat long')
+    >>> delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+    >>> delhi = City._make(delhi_data)
+    >>> delhi._asdict()
+    {'name': 'Delhi NCR', 'country': 'IN', 'population': 21.935, 'coordinates': LatLong(lat=28.613889, long=77.208889)}
+    >>> for key, value in delhi._asdict().items():
+    ...    print(key+':', value)
+    name: Delhi NCR
+    country: IN
+    population: 21.935
+    coordinates: LatLong(lat=28.613889, long=77.208889)
+    """
+    pass
+
+
 if __name__ == '__main__':
-    tuple_unpacking()
+    # tuple_unpacking()
+    named_tuple()
