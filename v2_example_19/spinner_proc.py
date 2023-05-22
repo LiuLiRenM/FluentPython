907bd1f852428302f66bf805a306eecd1a8a252f
Sakura <liuliren.mm@gmail.com>
2023-05-22 23:28:47 +0800

    添加使用进程实现旋转指针的例子

---
+++ v2_example_19/spinner_proc.py
@@ -0,0 +1,46 @@
+"""
+《流畅的python》第二版 19章第四节的第二个例子
+使用进程实现旋转指针
+
+"""
+import itertools
+import time
+from multiprocessing import Event, synchronize, Process
+
+
+def spin(msg: str, done: synchronize.Event) -> None:
+    status = ''
+    for char in itertools.cycle(r'\|/-'):
+        status = f'\r{char} {msg}'
+        print(status, end='', flush=True)
+        # 设置了时间之后，程序会阻塞在这个方法上面，就不会向终端打印字符，以达到动画效果
+        # 0.1秒之后done.wait()返回False，然后程序继续打印字符
+        if done.wait(.1):
+            break
+    blanks = ' ' * len(status)
+    print(f'\r{blanks}\r', end='')
+
+
+def slow() -> int:
+    time.sleep(13)
+    return 42
+
+
+def supervisor() -> int:
+    done = Event()
+    spinner = Process(target=spin, args=('thinking', done))
+    print(f'spinner object: {spinner}')
+    spinner.start()
+    result = slow()
+    done.set()
+    spinner.join()
+    return result
+
+
+def main() -> None:
+    result = supervisor()
+    print(f'Answer: {result}')
+
+
+if __name__ == '__main__':
+    main()
